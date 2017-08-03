502e5cc0e973f949c1dca435d959bad6ef2e76b0
Luis Nesi <lenesi@hotmail.com>
2017-08-03 12:52:43 +0100

    update delta_v

--- delta_v/js/main.js
+++ delta_v/js/main.js
@@ -708,7 +708,7 @@ var GameOverState = (function (_super) {
         var gameOverText = new Phaser.BitmapText(this.game, 0, Game.globalHeight / 2, 'PT Mono', "GAME OVER", 40);
         gameOverText.x = (Game.globalWidth / 2) - gameOverText.width / 2;
         this.contentLayer.add(gameOverText);
-        var startMessage = new Phaser.BitmapText(this.game, 0, Game.globalHeight - 200, 'PT Mono', "TOUCH TO START", 20);
+        var startMessage = new Phaser.BitmapText(this.game, 0, Game.globalHeight - 200, 'PT Mono', "PLAY AGAIN", 20);
         startMessage.x = (Game.globalWidth / 2) - startMessage.width / 2;
         var tween = this.game.add.tween(startMessage);
         tween.to({ alpha: 0.2 }, 500, "Linear", true, 0, -1, true);
